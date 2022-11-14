ec5cb399e13b0da0dcd928b4e3d6c7aeba4e301d
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2022-11-14 17:21:53 -0500

    part 3 task 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtuMbOHVLxh606Gy08CVZD4EaAz3Wv1S0",
+  authDomain: "bootcamp-charu.firebaseapp.com",
+  projectId: "bootcamp-charu",
+  storageBucket: "bootcamp-charu.appspot.com",
+  messagingSenderId: "872802402730",
+  appId: "1:872802402730:web:ea493610c0ddfd9b26abd3"
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <BrowserRouter>
