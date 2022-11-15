34aa53bc69538970bd1903c9f70b85670608be51
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2022-11-14 19:30:12 -0500

    part 3 task 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { initializeApp } from "firebase/app";
+import { Provider } from "react-redux";
+import firebase from 'firebase/compat/app';
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from "redux";
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from "react-redux-firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCtuMbOHVLxh606Gy08CVZD4EaAz3Wv1S0",
@@ -10,14 +17,36 @@ const firebaseConfig = {
   projectId: "bootcamp-charu",
   storageBucket: "bootcamp-charu.appspot.com",
   messagingSenderId: "872802402730",
-  appId: "1:872802402730:web:ea493610c0ddfd9b26abd3"
+  appId: "1:872802402730:web:ea493610c0ddfd9b26abd3",
 };
 
-initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+});
+
+// Create store with reducers and initial state
+const store = configureStore({reducer : rootReducer});
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+};
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById("root")
 );
